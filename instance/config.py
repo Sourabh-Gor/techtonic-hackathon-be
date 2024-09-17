698d67a94823c40e0fdc669e9bc53b78f0151439
Sourabh J Gor <sou7474@gmail.com>
2024-09-17 15:17:50 +0530

    Delete instance directory

--- instance/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://username:password@aws_rds_url:3306/hackathon_db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'myjwtsecret')
